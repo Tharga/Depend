fcabf79e93cb3077af23926f79e85310b1f8225a
Daniel Bohlin <daniel.bohlin@live.se>
2025-11-24 10:00:08 +0100

    move verification to chocolatey

--- Resources/choco/chocolateyUninstall.ps1
+++ Resources/choco/chocolateyUninstall.ps1
@@ -2,7 +2,7 @@ $toolsDir = Split-Path -Parent $MyInvocation.MyCommand.Definition
 $exePath  = Join-Path $toolsDir "depend.exe"
 
 if (Test-Path $exePath) {
-    Write-Host "Running unregister before uninstall..."
+    Write-Host "Unregistering depend by running: $exePath --unregister"
     & $exePath --unregister | Out-Null
 }
 else {
